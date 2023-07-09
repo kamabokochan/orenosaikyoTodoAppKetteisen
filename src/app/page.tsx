9f4d1a39a206646d40f61930f5fd1c4a292bd47b
kamabokochan <h.sasaki.worksfront@gmail.com>
2023-07-09 15:44:31 +0900

    簡単に画面生成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,40 @@
+"use client";
+
+import { useState } from "react";
+
+type List = {
+  label: string;
+};
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  const [text, setText] = useState("");
+  const [list, setList] = useState<List[]>([]);
+
+  const addTodo = () => {
+    setList([
+      ...list,
+      {
+        label: text,
+      },
+    ]);
+    setText("");
+  };
+
+  return (
+    <>
+      <div>
+        <input
+          type="text"
+          onChange={(e) => setText(e.target.value)}
+          value={text}
+        />
+        <button onClick={() => addTodo()}>追加</button>
+      </div>
+      <ul>
+        {list.map((item, i) => {
+          return <li key={i}>{item.label}</li>;
+        })}
+      </ul>
+    </>
+  );
 }

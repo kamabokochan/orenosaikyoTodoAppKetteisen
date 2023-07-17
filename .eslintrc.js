fd78a65f23d171ef4e6c016935b5a064ecc10f13
kamabokochan <h.sasaki.worksfront@gmail.com>
2023-07-17 20:31:10 +0900

    jest react-testing-libの導入

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
   extends: ["prettier"],
+  parserOptions: {
+    // https://typescriptbook.jp/tutorials/eslint#sourcetype
+    sourceType: "module",
+    // https://eslint.org/blog/2021/07/eslint-v7.30.0-released/#main
+    ecmaVersion: "latest",
+  },
 };

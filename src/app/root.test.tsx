49e739b58d1b035ac6c9e1aacfd078ac6b25ebe3
kamabokochan <h.sasaki.worksfront@gmail.com>
2023-07-22 19:43:46 +0900

    インテグレーションテストの検証

--- src/app/root.test.tsx
+++ src/app/root.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { composeStories } from "@storybook/testing-react";
 import * as stories from "./root.stories";
+import "@testing-library/jest-dom/extend-expect";
 
 const { AddTwoTodosAndCheckOneScenario } = composeStories(stories);
 
@@ -10,21 +11,26 @@ describe("play AddTwoTodosAndCheckOneScenario", () => {
     const { container } = render(<AddTwoTodosAndCheckOneScenario />);
 
     // Act
+    // TODO 下記のissueと同様の事象か？要解消
+    // https://github.com/storybookjs/storybook/issues/21573
+    // @ts-ignore
     await AddTwoTodosAndCheckOneScenario.play({ canvasElement: container });
 
     // Assert
     const todos = screen.getAllByTestId("todo-label");
     expect(todos).toHaveLength(2);
   });
+  // TODO 保留
   // it("checks one todo", async () => {
-  //     // Arrange
-  //     const {container} = render(<AddTwoTodosAndCheckOneScenario/>);
+  //   // Arrange
+  //   const { container } = render(<AddTwoTodosAndCheckOneScenario />);
 
-  //     // Act
-  //     await AddTwoTodosAndCheckOneScenario.play({canvasElement: container});
+  //   // Act
+  //   await AddTwoTodosAndCheckOneScenario.play({ canvasElement: container });
 
-  //     // Assert
-  //     const todoCheckboxes = screen.getAllByRole("checkbox", {checked: true});
-  //     expect(todoCheckboxes).toHaveLength(1);
-  // })
+  //   // Assert
+  //   const todos = screen.getAllByTestId("todo-label")[0];
+  //   console.log(todos);
+  //   // expect(todos).toHaveClass("label", "done");
+  // });
 });
